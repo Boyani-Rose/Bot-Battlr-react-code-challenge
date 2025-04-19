b796332d5505a2935789c0641667e53931ca5cc3
Rose Momanyi <rosebmomanyi@gmail.com>
2025-04-19 14:22:30 +0300

    Set up fetch  request using useEffect

--- src/App.js
+++ src/App.js
@@ -1,10 +1,25 @@
-
+import {useState, useEffect} from 'react'
 import './App.css';
 
 function App() {
+
+    const [bots, setBots] = useState([])
+    const [army, setArmy] = useState([])
+
+    useEffect(()=>{
+      fetch('http://localhost:8001/bots')
+      .then((response)=>response.json())
+      .then((data)=>setBots(data))
+    }, [])
+
   return (
-    <>
-    </>
+    
+    <div>
+      <YourBotArmy army={army} />
+      <BotCollection bots={bots} />
+    </div>
+
+   
    
   );
 }
